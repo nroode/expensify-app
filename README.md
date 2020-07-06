f549d0c8b5d4ed177a2c73e78e6e3e855a533983
Natalie Roode <nroode@gmail.com>
2020-07-05 17:44:55 -0700

    set up react router, initial routes

---
+++ README.md
@@ -0,0 +1,4 @@
+# IndecisionApp
+Utilized React, Scss, Flexbox to build this twist on a traditional task list app
+
+Built this app as part of Andrew Mead's React course
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+//need to specify entry and output
+//properties outline in webpack documentation webpack.js.org
+
+
+
+const path = require('path');
+
+
+console.log(path.join(__dirname, 'public'));
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true,
+    }
+};
